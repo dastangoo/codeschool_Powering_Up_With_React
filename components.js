e308c86cab127d3d0b45906caf12d2819e3f4546
dastangoo <hamed.dastangoo@gmail.com>
2018-01-24 13:07:28 +0330

    Talk Through Props. Building an App

--- components.js
+++ components.js
@@ -1,6 +1,45 @@
 class StoryBox extends React.Component {
   render() {
-    return (<div>Story Box</div>);
+    
+    const now = new Date();
+    const topicList = ['HTML', 'JavaScript', 'React'];
+    
+    return (
+      <div>
+        <h3>Story Box</h3>
+        <p className="lead">
+          Current time: {now.toTimeString()}
+        </p>
+        
+        <ul>
+          {topicList.map( topic => <li>{topic}</li> )}
+        </ul>
+      </div>
+    );
+  }
+} 
+
+
+ReactDom.render(
+  <StoryBox />, document.getElementById('story-app')
+);class StoryBox extends React.Component {
+  render() {
+    
+    const now = new Date();
+    const topicList = ['HTML', 'JavaScript', 'React'];
+    
+    return (
+      <div>
+        <h3>Story Box</h3>
+        <p className="lead">
+          Current time: {now.toTimeString()}
+        </p>
+        
+        <ul>
+          {topicList.map( topic => <li>{topic}</li> )}
+        </ul>
+      </div>
+    );
   }
 } 
 
