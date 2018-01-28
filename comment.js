7d2fd11f0b241ac90002d22d0ac536c80c077a24
dastangoo <hamed.dastangoo@gmail.com>
2018-01-28 10:35:53 +0330

    Talking to Remote Servers. Adding and Deleting Comments on the Server Side

--- comment.js
+++ comment.js
@@ -7,11 +7,20 @@ class Comment extends React.Component {
             {this.props.body}
           </p>
           <div className="comment-footer">
-            <a href="#" className="comment-footer-delete">
+            {/* When a user clicks on the link, the onClick event is emitted */}
+            <a href="#" className="comment-footer-delete" onClick={this._handleDelete.bind(this)}> 
               Delete comment
             </a>
           </div>
         </div>
       );
-  }
+  } // render
+  
+  _handleDelete(event) {
+    event.preventDefault();
+    if (confirm('Are you sure?')) {
+      // Show cofirmaiton box before deleting
+      this.props.onDelete(this.props.comments); // Call onDelete prop when button is clicked
+    }
+  } // _handleDelete
 }
--- commentbox.js
+++ commentbox.js
@@ -13,11 +13,12 @@ class CommentBox extends React.Component {
       // ];
       comments: []; /* Initialized to an empty array */
     }
-  }
+  } // Constructor
   
   componentWillMount() {
     _fetchComments(); /* Fetch comments from server before component is rendered */
-  }
+  } // componentWillMount
+  
   render() {
     const comments = this._getComments();
     return (
@@ -31,17 +32,18 @@ class CommentBox extends React.Component {
         <CommentForm addComment={this._addComment.bind(this)} />
       </div>
     );
-  }
+  } // render
   
   componentDidMount() {
     // Polling the server every five secodns
     // Store time as object property
     this._timer = setInterval(() => this._fetchComments(), 5000);
-  }
+  } // componentDidMount
   
   componenWillUnmount(){
     clearInterval(this._timer);
-  }
+  } // componentWillMount
+  
   _getComments(){
     // const commentList [
     //   { id: 1, author: "Morgan McCircuit", body: "Great picture!" },
@@ -50,9 +52,12 @@ class CommentBox extends React.Component {
     
     // Reading from component's state
     return this.state.comments.map((comment) => {
-      return (<Comment author={comment.author} body={comment.body} key={comment.id} />);
+      // return (<Comment author={comment.author} body={comment.body} key={comment.id} />);
+      return (<Comment comment={comment} 
+                       key={comment.id} 
+                       onDelete={this._deleteComments.bind(this)}/>); // Will later be called in the context of the CommentBox component
     });
-  }
+  } // _getComments
   
   _getCommentsTitle(commentCount) {
     if (commentCount === 0) {
@@ -62,20 +67,26 @@ class CommentBox extends React.Component {
     } else {
       return `${commentCount} comments`;
     }
-  }
+  } // _getCommentsTitle
   
   _addComment(author, body) {
       // Gets triggered by CommentForm when a new comment is added.
-      const comment = {
-        id: this.state.comments.length + 1,
-        author, 
-        body
-      };
+      // const comment = {
+      //   id: this.state.comments.length + 1,
+      //   author, 
+      //   body
+      // };
+      const comment = { author, body };
       
       // Updates state when function is called by adding new comment
-      this.setState({ comments: this.state.comments.concat([comment])}) /* New array references help React stary fast. So concat works better than push here */
-      
-  }
+      // this.setState({ comments: this.state.comments.concat([comment])}) /* New array references help React stary fast. So concat works better than push here */
+
+      jQuery.post('/api/comments', { comment })
+            .success(newComment => {
+              // State is only updated when we get the new comment from the API request
+              this.setState({ comments: this.state.comments.concat([newComment])}) 
+            });
+  } // _addComment
   
   // We can't call _fetchComments from render - we'll get an infinite loop
   // _fetchComments calls setState, which calls render
@@ -87,5 +98,23 @@ class CommentBox extends React.Component {
         this.setState({ comments })
       }
     });
-  }
-}
+  } // _fetchComments
+  
+  _deleteComments(comment) {
+    
+    // You can use jQuery or $ reference in jQuery library
+    $.ajax({
+      method: 'DELETE',
+      url: `/api/comments/${comment.id}`
+    });
+    
+    // Use spread operator to clone existing array
+    const comments = [...this.state.comments];
+    const commentIndex = comments.indexOf(comment);
+    // Removes comment from array
+    comments.splice(commentIndex, 1);
+    
+    // Updates state with modified comments array
+    this.setState({ comments });
+  } // _deleteComments
+} // CommentBox
